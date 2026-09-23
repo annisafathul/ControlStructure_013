f7e39a6e56302d8547fce69b59905d5ed6fc4cbe
annisafathul <annisafathul18@gmail.com>
2026-09-23 15:42:37 +0700

    program to evaluate the student performance

---
+++ PraktikumPertemuan2.py
@@ -0,0 +1,17 @@
+#NO.1
+grade = int(input("Masukkan Grade :"))
+
+if grade >= 90 :
+   print("Execellent")
+elif grade >= 80 :
+   print("Very Good")
+elif grade >= 70 :
+   print("Good")
+elif grade >= 60 :
+   print("Average")
+else :
+   print("poor")
+
+
+
+
